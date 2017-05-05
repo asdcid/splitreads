f2928a330cb6c0d677579b469019649315120ba7
asdcid <u4987172@anu.edu.au>
2017-05-05 13:24:57 +1000

    Update splitReads_single.sh

--- splitReads_single.sh
+++ splitReads_single.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #############################################################
-#dir contains single read files in fastq.gz format (the end of file must be ".fastq.gz", if not, change the for loop)
-inputDir='pacbio/'
-outputDir='separate'
-test_percentage=25 #percentage of data to reserve as test data (must be int!!!)
+#dir contains single read files in fastq.gz format (the end of file must be ".fastq.gz" or ".fq.gz")
+inputDir=$1
+outputDir=$2
+test_percentage=${3:-10} #percentage of data to reserve as test data (must be integer!!!)
 #############################################################
 
 #if this script has every error, exit
@@ -66,10 +66,64 @@ function split()
 }
 
 
+#help info
+if [ $# -lt 1 ]
+then
+  echo "Usage: ./splitReads_paried.sh inputDir outputDir  integer (1-99, default 10)"
+  exit 1
+elif [ $1 == --help -o $1 == -h ]
+then
+  echo "Usage: ./splitReads_paried.sh inputDir outputDir  integer (1-99, default 10)"
+  exit 1
+fi
 
-for R1gz in $inputDir/*fastq.gz
+
+#check argument
+if [ ! -d $inputDir ]
+then
+    echo "[ERROR]" $inputDir " is not a directory"
+    echo "Usage: ./splitReads_paried.sh inputDir outputDir integer (1-99, default 10)"
+    exit 1
+fi
+
+if [ ! -d $outputDir ]
+then
+    echo "[ERROR]" $outputDir " is not a directory"
+    echo "Usage: ./splitReads_paried.sh inputDir outputDir integer (1-99, default 10)"
+    exit 1
+fi
+
+if ! [[ $test_percentage =~ ^[0-9]+$ ]]
+then
+    echo "[ERROR] Percentage should be 1-99 (integer)"
+    echo "Usage: ./splitReads_paried.sh inputDir outputDir integer (1-99, default 10)"
+    exit 1
+elif [ $test_percentage -ge 100 -o $test_percentage -le 0 ]
+then
+    echo "[ERROR] Percentage should be 1-99 (integer)"
+    echo "Usage: ./splitReads_paried.sh inputDir outputDir integer (1-99, default 10)"
+    exit 1
+fi
+
+
+
+##begin
+
+
+for R1gz in $inputDir/*
 do
     echo 'Processing' $R1gz
+    if [[ $R1gz == *.fastq.gz ]]
+    then
+        extension=".fastq.gz"
+    elif [[ $R1gz == *.fq.gz ]]
+    then
+        extension=".fq.gz"
+    else
+        echo "[ERROR] The file is neither in fastq.gz or fq.gz format"
+        exit 1
+    fi
+    
     outputR1=$outputDir/$(basename $R1gz ".fastq.gz")
 
     #unzip fq for convenience
